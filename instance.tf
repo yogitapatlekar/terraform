b674c7900ab21c4c8d999e038de2d5018cbfc157
sharadrathod <156213379+sharadrathod@users.noreply.github.com>
2024-05-28 17:28:44 +0530

    ss

--- instance.tf
+++ instance.tf
@@ -5,13 +5,14 @@
  }
 */
 
-provider "aws"{
-    region = "us-east-1" 
+provider "aws" {
+  region = "us-east-1"
+}
 
-} 
-resource "aws_instance" "instance"{
-    ami = "ami-02bf8ce06a8ed6092"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = ["sg-09f186e51cf491527","sg-0dfe9ecea3b03dca3"]
-    key_name   = "1" 
-} 
+resource "aws_instance" "instance" {
+  ami                    = "ami-02bf8ce06a8ed6092"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-09f186e51cf491527", "sg-0dfe9ecea3b03dca3"]
+  key_name               = "1"
+}
+
