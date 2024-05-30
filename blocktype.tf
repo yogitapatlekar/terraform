131e4b9a85d698f6655dd18452ec4962ddf7e5ed
sharadrathod <156213379+sharadrathod@users.noreply.github.com>
2024-05-30 15:34:55 +0530

    ss

--- blocktype.tf
+++ blocktype.tf
@@ -1,12 +1,13 @@
 provider "aws" {
-  region = "us-east-1" 
-} 
+  region = "us-east-1"
+}
 
 terraform {
-  backend "s3" 
-   bucket = "cbz-online-terraform"
+  backend "s3" {
+    bucket = "cbz-online-terraform"
     key    = "terraform.tfstate"
     region = "us-east-1"
+  }
 }
 
 resource "aws_security_group" "allow_tls" {
@@ -15,18 +16,18 @@ resource "aws_security_group" "allow_tls" {
   vpc_id      = "vpc-006b120a1f8897bc9" # VPC ID should be a string
 
   ingress {
-    description      = "SSH rule"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH rule"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -62,7 +63,7 @@ variable "instance_type" {
   default = "t4g.micro" # Changed to an arm64 compatible instance type
 }
 
-output "hellow" {
+output "hello" {
   value = "hello world" # Correct spelling
 }
 
