1402801d51f244b411c8bcb140c3c6372da65c7f
sharadrathod <156213379+sharadrathod@users.noreply.github.com>
2024-05-31 17:17:53 +0530

    ss

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -1,59 +1,61 @@
 resource "aws_vpc" "this_vpc" {
-    cidr_block = var.this_vpc_cidr_block //"12.11.0.0/16"
-    tags =  {
-        Name = var.this_vpc_tags //"this_vpc"
-    }
+  cidr_block = var.this_vpc_cidr_block // "12.11.0.0/16"
+  tags = {
+    Name = var.this_vpc_tags // "this_vpc"
+  }
 }
 
 resource "aws_subnet" "this_subnet_pub" {
-  vpc_id     = aws_vpc.this_vpc.id  
-  availability_zone = "us-east-1"
-  cidr_block = var.this_subnet_pub_cidr_block      //"12.11.0.0/17"  
-  map_public_ip_on_launch = var.this_subnet_pub_map_ip //true
+  vpc_id                  = aws_vpc.this_vpc.id  
+  availability_zone       = "us-east-1"
+  cidr_block              = var.this_subnet_pub_cidr_block // "12.11.0.0/17"
+  map_public_ip_on_launch = var.this_subnet_pub_map_ip // true
   tags = {
-    Name = var.this_subnet_pub_tags //"pub_subnet"
+    Name = var.this_subnet_pub_tags // "pub_subnet"
   }
 }
+
 resource "aws_subnet" "this_subnet_private1" {
-  vpc_id     = aws_vpc.this_vpc.id
-  availability_zone = "us-east-1"
-  cidr_block = var.this_subnet_private1_cidr_block   //"12.11.128.0/19"
-  map_public_ip_on_launch = var.this_subnet_private_map_ip //false
+  vpc_id                  = aws_vpc.this_vpc.id
+  availability_zone       = "us-east-1"
+  cidr_block              = var.this_subnet_private1_cidr_block // "12.11.128.0/19"
+  map_public_ip_on_launch = var.this_subnet_private_map_ip // false
   tags = {
-    Name = var.this_subnet_private1_tags //"pri_subnet1"
+    Name = var.this_subnet_private1_tags // "pri_subnet1"
   }
 }
+
 resource "aws_subnet" "this_subnet_private2" {
-  vpc_id     = aws_vpc.this_vpc.id
-  availability_zone = "us-east-1"
-  map_public_ip_on_launch = var.this_subnet_private_map_ip //false
-  cidr_block = var.this_subnet_private2_cidr_block //"12.11.192.0/20"
-
+  vpc_id                  = aws_vpc.this_vpc.id
+  availability_zone       = "us-east-1"
+  cidr_block              = var.this_subnet_private2_cidr_block // "12.11.192.0/20"
+  map_public_ip_on_launch = var.this_subnet_private_map_ip // false
   tags = {
-    Name = var.this_subnet_private2_tags //"pri_subnet2"
+    Name = var.this_subnet_private2_tags // "pri_subnet2"
   }
 }
+
 resource "aws_internet_gateway" "this_igw" {
-    vpc_id = aws_vpc.this_vpc.id
-    tags = {
-        Name = var.this_igw_tags
-    }
+  vpc_id = aws_vpc.this_vpc.id
+  tags = {
+    Name = var.this_igw_tags
+  }
 }
 
-resource "aws_default_route_table" "example" {
-  default_route_table_id = aws_vpc.this_vpc.default_route_table_id
+resource "aws_route_table" "this_route_table" {
+  vpc_id = aws_vpc.this_vpc.id
 
   route {
-    cidr_block = var.this_def_Route_example_cidr_block //"0.0.0.0/0"
+    cidr_block = var.this_def_Route_example_cidr_block // "0.0.0.0/0"
     gateway_id = aws_internet_gateway.this_igw.id
   }
 
   tags = {
-    Name = var.this_def_Route_example_tag //"default"
+    Name = var.this_def_Route_example_tag // "default"
   }
 }
 
 resource "aws_route_table_association" "this_rw_association" {
   subnet_id      = aws_subnet.this_subnet_pub.id
-  route_table_id = aws_default_route_table.example.id
+  route_table_id = aws_route_table.this_route_table.id
 }
