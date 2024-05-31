dc471dd0fb38d0ebb9388a42c4a8dadbb7178848
sharadrathod <156213379+sharadrathod@users.noreply.github.com>
2024-05-31 13:04:49 +0530

    aa

--- demo/instance.tf
+++ demo/instance.tf
@@ -2,13 +2,9 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" {
-    
+resource "aws_instance" "aws_instance" {
   ami  = "ami-00beae93a2d981137"
   instance_type = "t3.micro"
-
   key_name = "1" 
-
   security_groups = ["sg-0c4bf15d5abd926d2"]
-
 }
