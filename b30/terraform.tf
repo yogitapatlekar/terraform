dffd8c8fd098f6a305062c7cc9eff3f3b1adaae7
yogitapatlekar <166595728+yogitapatlekar@users.noreply.github.com>
2024-06-03 13:04:30 +0530

    qq

--- b30/terraform.tf
+++ b30/terraform.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 terraform {
   backend "s3" {
     bucket = "cbz-online-terraform"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
 
