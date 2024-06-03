2f159f858ebf0de0fbb6f9b348d139bb92ab4609
yogitapatlekar <166595728+yogitapatlekar@users.noreply.github.com>
2024-06-03 22:47:38 +0530

    aa

--- s3/s3.tf
+++ s3/s3.tf
@@ -1,49 +1,56 @@
 provider "aws" {
-  region = "us-west-1"
+  region = "us-east-1"  # Replace with your desired region
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-12345678"
+# Variables
+variable "bucket_name" {
+  description = "The name of the S3 bucket to create"
+  type        = string
+}
 
-  tags = {
-    Name        = "MyBucket"
-    Environment = "Dev"
+# Create S3 bucket
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = var.bucket_name
+
+  # Optional: Configure bucket versioning
+  versioning {
+    enabled = true
   }
-}
 
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.my_bucket.bucket
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "my_bucket_encryption" {
-  bucket = aws_s3_bucket.my_bucket.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+  # Optional: Configure bucket encryption
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
     }
   }
 }
 
-resource "aws_s3_bucket_policy" "my_bucket_policy" {
-  bucket = aws_s3_bucket.my_bucket.id
+# Set the bucket policy to make the entire bucket public
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.public_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action    = "s3:GetObject"
-        Effect    = "Allow"
-        Resource  = "${aws_s3_bucket.my_bucket.arn}/*"
+        Effect = "Allow"
         Principal = "*"
-      },
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.public_bucket.arn}/*"
+      }
     ]
   })
 }
 
-output "bucket_arn" {
-  value = aws_s3_bucket.my_bucket.arn
+# Output the bucket name and URL (optional, for verification)
+output "bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = aws_s3_bucket.public_bucket.bucket
+}
+
+output "bucket_url" {
+  description = "The URL of the S3 bucket"
+  value       = "http://${aws_s3_bucket.public_bucket.bucket}.s3.amazonaws.com"
 }
