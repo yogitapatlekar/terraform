2ff5ddd9baffbbc3845c2fe05ddfff8a7c3ddb67
yogitapatlekar <166595728+yogitapatlekar@users.noreply.github.com>
2024-06-03 13:58:03 +0530

    dd

--- b30/terraform.tf
+++ b30/terraform.tf
@@ -13,7 +13,7 @@ terraform {
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow SSH for all network"
-  vpc_id      = "vpc-006b120a1f8897bc9" # VPC ID should be a string
+  vpc_id      = "vpc-0922d23ff557a0bb5" # VPC ID should be a string
 
   ingress {
     description = "SSH rule"
