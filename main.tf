3d7e71f3b3da9fb4d7cdd47c0fca45edd8ee42de
yogitapatlekar <166595728+yogitapatlekar@users.noreply.github.com>
2024-06-04 13:35:59 +0530

    gg

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "ec2" {
     source = "./module/ec2"
     this_aws_instance_ami = "ami-00beae93a2d981137"
     this_aws_instance_instance_type  = "t2.micro"
-    this_aws_instance_subnet = module.aws_vpc.aws_subnet_this_public
+    //this_aws_instance_subnet = module.aws_vpc.aws_subnet_this_public
     this_aws_instance_key_name = "me"
     this_aws_instance_availability_zone = "us-east-1a"
     this_aws_instance_volume_size = 15
