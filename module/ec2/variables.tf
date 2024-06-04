7786d39354d7c96dfd7db36533de52205ecb5033
yogitapatlekar <166595728+yogitapatlekar@users.noreply.github.com>
2024-06-04 13:29:08 +0530

    aa

---
+++ module/ec2/variables.tf
@@ -0,0 +1,31 @@
+variable "this_aws_instance_ami" {
+     type = string
+}
+variable "this_aws_instance_instance_type" {
+     type = string
+}
+variable "this_aws_instance_key_name" {
+     type = string
+}
+
+variable "this_aws_instance_availability_zone" {
+    type = string
+}
+variable "this_aws_instance_subnet " {
+    type = string
+}
+variable "this_aws_instance_volume_size" {
+    type = string
+}
+variable "this_aws_instance_count" {
+    type = string
+}
+
+variable "this_aws_instance_tags" {
+    type = string
+} 
+
+variable "this_sg_vpc_id" {
+   type = string
+}
+
