3e09c534e202334f7d990ed83dff35e7e22e05aa
yogitapatlekar <166595728+yogitapatlekar@users.noreply.github.com>
2024-06-04 13:08:05 +0530

    aa

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "vpc" {
 
 module "ec2" {
     source = "./module/ec2"
-    this_aws_instance_ami = "ami-0eb5115914ccc4bc2"
+    this_aws_instance_ami = "ami-00beae93a2d981137"
     this_aws_instance_instance_type  = "t2.micro"
     //this_aws_instance_subnet = module.aws_vpc.aws_subnet_this_public
     this_aws_instance_key_name = "delete_oregon_anup"
