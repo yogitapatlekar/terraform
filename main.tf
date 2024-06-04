8f7adb84896070f01fec211a3f9db57a51c734e2
yogitapatlekar <166595728+yogitapatlekar@users.noreply.github.com>
2024-06-04 13:14:49 +0530

    aa

--- main.tf
+++ main.tf
@@ -26,9 +26,9 @@ module "ec2" {
     this_aws_instance_instance_type  = "t2.micro"
     //this_aws_instance_subnet = module.aws_vpc.aws_subnet_this_public
     this_aws_instance_key_name = "me"
-    this_aws_instance_availability_zone = "us-east-1b"
+    this_aws_instance_availability_zone = "us-east-1a"
     this_aws_instance_volume_size = 15
-    this_aws_instance_count = 1
+    this_aws_instance_count = 2
     this_aws_instance_user_data_base64 = true
     this_aws_instance_tags =  "Terraform_instance"
     this_aws_instance_subnet = module.vpc.subnet_id
