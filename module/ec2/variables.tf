0d8eed525f60c10adba93c9c146b2ae246e87901
yogitapatlekar <166595728+yogitapatlekar@users.noreply.github.com>
2024-06-04 13:30:27 +0530

    dd

--- module/ec2/variables.tf
+++ module/ec2/variables.tf
@@ -11,7 +11,7 @@ variable "this_aws_instance_key_name" {
 variable "this_aws_instance_availability_zone" {
     type = string
 }
-variable "this_aws_instance_subnet " {
+variable "this_aws_instance_subnet" {
     type = string
 }
 variable "this_aws_instance_volume_size" {
